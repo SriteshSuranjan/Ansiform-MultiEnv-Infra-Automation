6616ae43c89e4dbd48cec100df1a2538bba91918
root <root@SRITESH-SURANJAN.>
2025-02-20 09:45:30 +0000

    first commit

--- vpc-terraform/terraform/infrastructure/s3.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "main" {
-  bucket = "${var.env}-env-ansiform-iaac-bucket-77"
-  tags = {
-    Name = "${var.env}-env-ansiform-iaac-bucket-77"
-    environment = var.env
-  }
-}
